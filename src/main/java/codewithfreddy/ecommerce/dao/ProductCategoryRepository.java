5634cc2d6c8a636a4e23a9a2da26eb1a30be0d9d
Thabang <rangataft19@gmail.com>
2023-10-15 14:10:11 +0200

    product and category

---
+++ src/main/java/codewithfreddy/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package codewithfreddy.ecommerce.dao;
+
+import codewithfreddy.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product_category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
---
+++ src/main/java/codewithfreddy/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,7 @@
+package codewithfreddy.ecommerce.dao;
+
+import codewithfreddy.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
